0fce2031d1b012ec2c364b94397b3ad30a7a0028
mason <mason.mirabito750@schools.sa.edu.au>
2026-05-04 14:23:16 +0930

    initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+class Vector {
+  constructor(x, y) {
+    this.x = x | 0
+    this.y = y | 0
+  }
+}
+
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+const dimensions = new Vector(canvas.width, canvas.height)
+const center = new Vector(dimensions.x / 2, dimensions.y / 2);
+console.log(center);
+
+ctx.fillStyle = "rgb(200 0 0)";
+ctx.fillRect(center.x - 25, center.y - 25, 50, 50);
+
+ctx.fillStyle = "rgb(0 0 200 / 50%)";
+ctx.fillRect(10 + center.x - 25, 10 + center.y - 25, 50, 50);
+
+function generateMaze() {
+  /*
+  given a grid of cells. use recursive division to 
+  create a maze where a cell can either be a wall or open.
+  */
+}
